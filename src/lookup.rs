33ce3896010d9e38b729c9e23065a13893117658
David Ventura <david@davidv.dev>
2025-08-27 23:16:49 +0200

    add pretty print

--- src/lookup.rs
+++ src/lookup.rs
@@ -1,16 +1,36 @@
 use std::time::Instant;
 
+use tarkka::AggregatedWord;
+
 mod reader;
+
+fn pretty_print(wn: &str, w: AggregatedWord) {
+    let ipa = match w.ipa_sound {
+        Some(ref v) if v.len() > 0 => v.first().unwrap(),
+        _ => "",
+    };
+
+    let hyphenation = match w.hyphenation {
+        Some(ref h) if h.len() > 0 => h.first().unwrap(),
+        _ => "",
+    };
+    println!("{wn} - {} - {}", ipa, hyphenation);
+    for pg in w.pos_glosses {
+        println!("{}:", pg.pos);
+        for glos in pg.glosses {
+            println!(" - {}", glos);
+        }
+    }
+}
 
 fn main() {
     let s = Instant::now();
     let mut d = reader::DictionaryReader::open("dictionary.dict").unwrap();
     println!("read {:?}", s.elapsed());
     let s = Instant::now();
-    let r = d.lookup("Alemania").unwrap();
+    let r = d.lookup("Austria").unwrap();
     println!("looked 1st up {:?}", s.elapsed());
-    println!("word = {r:?}");
-    let r = d.lookup("hola").unwrap();
-    println!("looked 2nd up {:?}", s.elapsed());
-    println!("word = {r:?}");
+    if let Some(w) = r {
+        pretty_print("cane", w);
+    };
 }
